37eec8201ce7c8f9cd9028ef3266558c259277d0
Maryus Martsyalis <martsyalis@gmail.com>
2021-09-16 18:30:37 -0600

    fix distructuring syntax

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const ramda, {curry} = require('ramda');
+const { curry, ...ramda } = require("ramda");
 
 const asyncCompose =
-  (...functions) =>
-  (input) =>
-    functions.reduceRight(
-      (chain, func) => chain.then(func),
-      Promise.resolve(input)
-    );
+    (...functions) =>
+    input =>
+        functions.reduceRight(
+            (chain, func) => chain.then(func),
+            Promise.resolve(input)
+        );
 
 const trace = curry((message, arg) => {
-  console.log(`${message}: `, arg);
-  return arg;
+    console.log(`${message}: `, arg);
+    return arg;
 });
 
 const mapPromises = async (promises, cb) =>
-  !promises.length
-    ? []
-    : [await cb(head(promises)), ...(await mapPromises(tail(promises), cb))];
+    !promises.length
+        ? []
+        : [
+              await cb(head(promises)),
+              ...(await mapPromises(tail(promises), cb)),
+          ];
 
 module.exports = {
- ...ramda,
-  asyncCompose,
-  trace,
-  mapPromises,
+    ...ramda,
+    asyncCompose,
+    trace,
+    mapPromises,
 };
